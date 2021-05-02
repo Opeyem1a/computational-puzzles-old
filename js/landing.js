91b6ebaa93e93419d819f1f71a36a2dd7eaaae77
opeyem1a <opeyadeyemi@gmail.com>
2021-05-01 17:48:39 -0700

    - pivot based on feedback + update style for site + added hint reveal functionality + cleaned up some styling errors

--- js/landing.js
+++ js/landing.js
@@ -3,7 +3,9 @@ const PUZZLES_DIR = `./puzzles`;
 $(function () {
     $('#puzzles-drawer').children().each((i, el) => {
         $(el).on('click', () => {
-            routeToPuzzle(i + 1);
+            // routeToPuzzle(i + 1);
+            console.log(el);
+            showPuzzleHint(el);
         })
     });
 });
@@ -11,4 +13,13 @@ $(function () {
 
 const routeToPuzzle = (puzzleNum) => {
     window.location.href = `${PUZZLES_DIR}/puzzle${puzzleNum}.html`;
+};
+
+const showPuzzleHint = (el) => {
+    $(el)?.toggleClass('selected');
+
+    const descDiv = $(el)?.find('div.puzzle-description');
+    descDiv?.find('p')?.toggleClass('d-none');
+    descDiv?.find('small.hint-reminder')?.toggleClass('d-none');
+    descDiv?.find('small').toggleClass('text-muted');
 };
